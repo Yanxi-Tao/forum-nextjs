207d41cea2075d9a955b71fa90c310f8044d8b98
Yanxi Tao <yanxi_tao@fis.edu>
2024-03-03 10:47:43 +0100

    DraggableBlockPlugin utils init

---
+++ components/editor/plugins/DraggableBlockPlugin/index.jsx
@@ -0,0 +1,5 @@
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import { eventFiles } from '@lexical/rich-text'
+import { mergeRegister } from '@lexical/utils'
+
+import * as React from 'react'
