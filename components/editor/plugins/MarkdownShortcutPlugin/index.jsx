ed24c4e670d84a5461c941cca8ec5704c5e5ba45
Yanxi-Tao <yanxi_tao@fis.edu>
2024-03-10 01:13:30 +0100

    clean up

--- components/editor/plugins/MarkdownShortcutPlugin/index.jsx
+++
@@ -1,7 +0,0 @@
-import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin'
-import * as React from 'react'
-import { EDITOR_TRANSFORMERS } from './MarkdownTransformers'
-
-export default function MarkdownPlugin() {
-  return <MarkdownShortcutPlugin transformers={EDITOR_TRANSFORMERS} />
-}
