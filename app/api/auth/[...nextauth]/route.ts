8d5f471772ea2f20fe663a31209cb42d68849816
Yanxi-Tao <yanxi_tao@fis.edu>
2024-03-20 20:49:02 +0100

    next-auth setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,4 @@
+export { GET, POST } from '@/auth'
+
+// prisma does not support edge runtime
+// export const runtime = "edge"
---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import authConfig from '@/auth.config'
+import NextAuth from 'next-auth'
+
+const { auth } = NextAuth(authConfig)
+
+export default auth((req) => {
+  // req.auth
+  const isLoggedIn = !!req.auth
+  console.log(req.nextUrl)
+})
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: [
+    // Exclude files with a "." followed by an extension, which are typically static files.
+    // Exclude files in the _next directory, which are Next.js internals.
+
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    // Re-include any files in the api or trpc folders that might have an extension
+    '/(api|trpc)(.*)',
+  ],
+}
