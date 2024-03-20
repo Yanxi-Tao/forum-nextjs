0c12cd498217dba55e155fe113968dbdc4cb8050
Yanxi-Tao <yanxi_tao@fis.edu>
2024-03-20 21:30:58 +0100

    login action setup

--- actions/auth/login.ts
+++ actions/auth/login.ts
@@ -1,7 +1,10 @@
 'use server'
 
 import { z } from 'zod'
+import { signIn } from '@/auth'
 import { LoginSchema } from '@/schemas'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
+import { AuthError } from 'next-auth'
 
 export const login = async (data: z.infer<typeof LoginSchema>) => {
   const validatedData = LoginSchema.safeParse(data)
@@ -10,8 +13,24 @@ export const login = async (data: z.infer<typeof LoginSchema>) => {
     return { type: 'error', message: 'Invalid data' }
   }
 
-  return {
-    type: 'success',
-    message: 'Verification email sent',
+  const { email, password } = validatedData.data
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { type: 'error', message: 'Invalid credentials' }
+        default:
+          return { type: 'error', message: 'An error occurred' }
+      }
+    }
+
+    throw error
   }
 }
--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,3 +1,18 @@
-export default function PrivatePage() {
-  return <div>PrivatePage</div>
+import { auth, signOut } from '@/auth'
+
+export default async function PrivatePage() {
+  const session = await auth()
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          'use server'
+          await signOut()
+        }}
+      >
+        <button type="submit">sign out</button>
+      </form>
+    </div>
+  )
 }
