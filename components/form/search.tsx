7efab4f601441109c0d1a71e180a57f6399d3979
Yanxi-Tao <yanxi_tao@fis.edu>
2024-03-28 03:10:41 +0100

    search function improved

--- components/form/search.tsx
+++ components/form/search.tsx
@@ -1,40 +1,17 @@
 'use client'
 import { Input } from '@/components/ui/input'
 import { SearchIcon, X } from 'lucide-react'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useCallback } from 'react'
+import { useSearchParams } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 
 export const Search = () => {
-  const router = useRouter()
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  // Create a query string with the search parameter
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString())
-      params.set(name, value)
-
-      return params.toString()
-    },
-    [searchParams]
-  )
-
-  // Handle the search form submission
-  // update the search query parameter
-  const onSearch = (data: FormData) => {
-    const search = data.get('search') as string
-    const query = createQueryString('search', search)
-    router.push(`${pathname}?${query}`)
-    router.refresh()
-  }
-
+  const search = useSearchParams().get('search') || ''
   return (
-    <form action={onSearch} className="flex items-center rounded-full border">
+    <form className="flex items-center rounded-full border">
       {/* todo: add filter badge */}
       <Input
         placeholder="Search"
+        defaultValue={search}
         className="w-96 focus-visible:ring-0 focus-visible:ring-offset-0 h-10 rounded-none rounded-l-full border-0"
         autoComplete="off"
         name="search"
