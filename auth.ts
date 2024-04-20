89b968536dc6c5f799a52edd4562ebf80aa731fd
Yanxi-Tao <yanxi_tao@fis.edu>
2024-04-21 00:45:20 +0200

    -

--- auth.ts
+++ auth.ts
@@ -64,7 +64,11 @@ export const {
       }
       await db.user.update({
         where: { id: user.id },
-        data: { emailVerified: new Date(), slug, profile: { create: {} } },
+        data: {
+          emailVerified: new Date(),
+          slug,
+          profile: { create: { bio: '' } },
+        },
       })
     },
   },
